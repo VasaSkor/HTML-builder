12ac885d6675c16c4a864e9a97bae91bbb431871
VasaSkor <106305964+VasaSkor@users.noreply.github.com>
2022-11-05 22:17:40 +0300

    feat: complete task '04-copy-directory'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,21 +2,26 @@ const fs = require('fs');
 const path = require('path');
 
 const copyFile = (sourcePath, targetPath) => {
-  fs.mkdir(targetPath, () => {});
-  fs.readdir(sourcePath,
-    { withFileTypes: true },
-    (err, files) => {
-      files.forEach((file) =>{
-        const readStream =
-          fs.createReadStream(path.resolve(sourcePath, file.name));
-        const writeStream =
-          fs.createWriteStream(path.resolve(targetPath, file.name));
+  fs.rmdir(targetPath, {
+    recursive: true
+  }, () => {
+    fs.mkdir(targetPath, () => {
+      fs.readdir(sourcePath,
+        { withFileTypes: true },
+        (err, files) => {
+          files.forEach((file) =>{
+            const readStream =
+              fs.createReadStream(path.resolve(sourcePath, file.name));
+            const writeStream =
+              fs.createWriteStream(path.resolve(targetPath, file.name));
 
-        readStream.on('data', (chunk) => {
-          writeStream.write(chunk);
+            readStream.on('data', (chunk) => {
+              writeStream.write(chunk);
+            });
+          });
         });
-      });
     });
+  });
 };
 
 copyFile(
